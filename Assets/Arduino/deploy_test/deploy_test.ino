a05e6168ce4638007043d028751431b35dc96d9d
Segze <96857972+Segze@users.noreply.github.com>
2023-06-25 15:47:28 -0600

    Literatura para 9DOF

--- Assets/Arduino/deploy_test/deploy_test.ino
+++ Assets/Arduino/deploy_test/deploy_test.ino
@@ -1,16 +1,29 @@
 // Incluímos la librería para poder controlar el servo
 #include <Servo.h>
+#include <Wire.h>
+#include <BMP280_DEV.h>
+
  
 // Declaramos la variable para controlar el servo
 Servo servoMotor;
 int initialAngle = 3;
 int finalAngle = 25;
 float seconds = 0;
-float deployTimeS = 20;
+float deployTimeS = 90;
 bool deployed = false;
 int servoPin = 5;
 int ledPin = 13;
+BMP280_DEV bmp280;
+float temperature,pressure,altitude;
+
 void setup() {
+  Serial.begin (9600);
+  bmp280.begin(BMP280_I2C_ALT_ADDR);
+  bmp280.setTimeStandby(TIME_STANDBY_2000MS);
+  bmp280.startNormalConversion();
+
+
+
   pinMode(ledPin,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
@@ -40,12 +53,15 @@ for (int i=0;i<5;i++){
 }
  
 void loop() {
+
   if (!deployed){
     while (seconds < deployTimeS){
       seconds = millis()/1000;
     }
     // Esperamos 1 segundo
     delay(1000);
+    digitalWrite(ledPin,HIGH);
+  
     
     // Desplazamos 
     servoMotor.write(finalAngle);
@@ -53,6 +69,14 @@ void loop() {
   }
   else {
     //Aqui se pueden guardar datos importantes dentro de la EEPROM, aunque por ahora no hay ningun dato relevante
+      if (bmp280.getMeasurements(temperature,pressure,altitude)){
+    Serial.print("TEMP");
+    Serial.println(temperature);
+        Serial.print("PRESS");
+    Serial.println(pressure);
+        Serial.print("ALTITUDE");
+    Serial.println(altitude);
+  }
 
   }
 
