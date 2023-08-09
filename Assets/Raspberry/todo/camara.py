195beb65de9c2d7907bf5fbd6ec2cdc6ed9b73b1
Alexz <ale.school.cecyt@gmail.com>
2023-08-09 20:11:37 +0100

    Modulo de camara

--- Assets/Raspberry/todo/camara.py
+++ Assets/Raspberry/todo/camara.py
@@ -2,22 +2,25 @@ from time import sleep
 from picamera import PiCamera
 from io import BytesIO
 
-
-camera = PiCamera(resolution=(1280, 720), framerate=30)
-# Set ISO to the desired value
-camera.iso = 100
-# Wait for the automatic gain control to settle
-sleep(2)
-# Now fix the values
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
-# Finally, take several photos with the fixed settings
-#camera.capture_sequence(['image%02d.jpg' % i for i in range(10)],resize=(320, 240))
+camera = None
+def initCamera():
+    global camera
+    camera = PiCamera(resolution=(1280, 720), framerate=30)
+    # Set ISO to the desired value
+    camera.iso = 100
+    # Wait for the automatic gain control to settle
+    sleep(2)
+    # Now fix the values
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+    # Finally, take several photos with the fixed settings
+    #camera.capture_sequence(['image%02d.jpg' % i for i in range(10)],resize=(320, 240))
 
 def capture():
+    global camera
     # Create the in-memory stream
     stream = BytesIO()
     camera.capture(stream, format='jpeg',resize=(320, 240))
