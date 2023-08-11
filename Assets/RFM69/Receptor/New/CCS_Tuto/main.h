f0a4fffce56d3b27a8e9533d3b360923019890cf
Segze <96857972+Segze@users.noreply.github.com>
2023-08-11 12:14:27 -0600

    Borre archivos viejos

--- Assets/RFM69/Receptor/New/CCS_Tuto/main.h
+++
@@ -1,7 +0,0 @@
-#include <18F4550.h>
-#device ADC=10
-
-#FUSES NOWDT                 	//No Watch Dog Timer
-
-#use delay(clock=24MHz,crystal=16MHz,USB_LOW)
-
--- Assets/RFM69/Receptor/New/Segundo_Round.c
+++
@@ -1,8 +0,0 @@
-void main() {
-
-UART1_Init(9600);
-while (1){
-UART1_Write_Text("Funciona");
-}
-
-}
