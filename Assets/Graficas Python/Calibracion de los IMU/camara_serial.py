460acecf2162c3449a1e3efbdafeca663532f908
Segze <96857972+Segze@users.noreply.github.com>
2023-08-16 04:55:20 -0600

    Prueba de la recepcion de imagen en python :snake:

---
+++ Assets/Graficas Python/Calibracion de los IMU/camara_serial.py
@@ -0,0 +1,42 @@
+import serial
+import os
+#from PIL import Image
+from io import BytesIO
+import cv2
+import time 
+
+datos = serial.Serial('COM8', 9600)  
+datos.readline()
+isImg = False
+imgLen = 0
+while True:
+    if (not isImg):
+        line = datos.readline()
+        arr = (str(line)[3:-4]).split(',')
+        try:
+            imgLen = int(arr[24])
+        except:
+            imgLen = -1
+        print (f"{arr}, {imgLen}")
+        if (imgLen > 0):
+            isImg = True
+    else:
+        print (f"{imgLen} bytes")
+        rawImg = datos.read(imgLen)
+        print (rawImg)
+        print (len(rawImg))
+        with open("test.jpg","wb") as f:
+            f.write(rawImg)
+        image = cv2.imread("test.jpg")
+        cv2.imshow("CanSat",image)
+        #Esperar un segundo
+        time.sleep(1.5)
+        cv2.destroyAllWindows()
+        
+        # bio = BytesIO(rawImg)
+        # img = Image.open("test.jpg")
+        # img.show()
+        # img.save("Foto.jpeg")
+        isImg = False
+        imgLen = 0
+
