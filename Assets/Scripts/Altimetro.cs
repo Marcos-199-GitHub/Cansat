27ff2b59c13a95da87b5397261945a4a8f744280
Marcos <75189483+Marcos-199-GitHub@users.noreply.github.com>
2024-10-22 20:13:51 -0600

    .

---
+++ Assets/Scripts/Altimetro.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class Altimetro : MonoBehaviour{
+    public string port = "COM6";
+    public int baudrate = 9600;
+
+    private SerialPort serialPort;
+    public DatosAltimetro DatosRecibidos = new DatosAltimetro();
+    public List<DatosAltimetro> DatosGuardados = new List<DatosAltimetro>();
+
+    public TextMeshProUGUI TextTemperatura;
+    public TextMeshProUGUI TextPresion;
+    public TextMeshProUGUI TextAltura;
+    public TextMeshProUGUI TextVelocidad;
+
+    public Graficador GraficaTemperatura;
+    public Graficador GraficaPresion;
+    public Graficador GraficaAltura;
+    public Graficador GraficaVelocidad;
+
+    public float t0 = 0;
+
+    private void Start(){
+        //serialPort = new SerialPort( PlayerPrefs.GetString(ComSelector.key), 9600 );
+        updatePort(port, baudrate);
+
+        GraficaTemperatura.setTituloYEtiquetas("Temperatura BMP180", "Tiempo [s]", "Temperatura [°C]");
+        GraficaPresion.setTituloYEtiquetas("Presión BMP180", "Tiempo [s]", "Presión [hPa]");
+        GraficaAltura.setTituloYEtiquetas("Altura BMP180", "Tiempo [s]", "Altura [m]");
+        GraficaVelocidad.setTituloYEtiquetas("Velocidad BMP180", "Tiempo [s]", "Velocidad [m/s]");
+
+        GraficaTemperatura.setYRangeAndXRange(0, 30);
+        GraficaPresion.setYRangeAndXRange(0, 1200);
+        GraficaAltura.setYRangeAndXRange(2000, 3000);
+        GraficaVelocidad.setYRangeAndXRange(-10, 10);
+
+        t0 = Time.time;
+    }
+
+    private void updatePort(string portn, int baudraten){
+        serialPort = new SerialPort(portn, baudraten);
+        serialPort.ReadTimeout = 5000;
+    }
+
+    private void OnDisable(){
+        serialPort.Close();
+    }
+
+    private void Update(){
+        if (serialPort.IsOpen){
+            string serialData = serialPort.ReadTo("\n");
+            if (serialData.Length == 0){
+                Debug.Log("No hay datos");
+                return;
+            }
+
+            // Procesa los datos recibidos (puedes realizar análisis y acciones aquí)
+            Debug.Log("Datos seriales recibidos: " + serialData);
+            //Si el mensaje son los datos
+            string[] data = serialData.Replace(" ", "").Split(',');
+            if (data.Length < 4){
+                Debug.Log("Datos incompletos");
+                return;
+            }
+
+            DatosRecibidos = new DatosAltimetro();
+            float.TryParse(data[0], out DatosRecibidos.temperatura);
+            float.TryParse(data[1], out DatosRecibidos.presion);
+            float.TryParse(data[2], out DatosRecibidos.altura);
+            float.TryParse(data[3], out DatosRecibidos.velocidad);
+            DatosGuardados.Add(DatosRecibidos);
+            
+            float time = Time.time - t0;
+            TextTemperatura.text = DatosRecibidos.temperatura + " °C";
+            TextPresion.text = DatosRecibidos.presion + " hPa";
+            TextAltura.text = DatosRecibidos.altura + " m";
+            TextVelocidad.text = DatosRecibidos.velocidad + " m/s";
+
+            GraficaTemperatura.agregarPunto(time, DatosRecibidos.temperatura);
+            GraficaPresion.agregarPunto(time, DatosRecibidos.presion);
+            GraficaAltura.agregarPunto(time, DatosRecibidos.altura);
+            GraficaVelocidad.agregarPunto(time, DatosRecibidos.velocidad);
+
+            return;
+        }
+
+        try{
+            Debug.Log("Tratando de abrir puerto serial");
+            serialPort.Open();
+            Debug.Log("Listo");
+        }
+        catch (Exception e){
+            Debug.Log(e);
+        }
+    }
+
+    public void saveData(){
+        string path = "Assets/Logs/log_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+        Debug.Log("Guardando datos en " + path);
+        using (StreamWriter sw = File.CreateText(path)){
+            foreach (DatosAltimetro d in DatosGuardados){
+                sw.WriteLine(d.temperatura + "," + d.presion + "," + d.altura + "," + d.velocidad);
+            }
+        }
+    }
+}
+
+[Serializable]
+public class DatosAltimetro{
+    public float temperatura;
+    public float presion;
+    public float altura;
+    public float velocidad;
+}
--- Assets/Scripts/Comunicacion.cs
+++ Assets/Scripts/Comunicacion.cs
@@ -6,31 +6,33 @@ using System.Threading;
 using UnityEngine;
 
 public class Comunicacion : MonoBehaviour{
-    SerialPort   serialPort;
+    public string port = "COM6";
+    public int baudrate = 9600;
+
+    private SerialPort serialPort;
     public Datos DatosRecibidos = new Datos();
 
-    public static bool   NuevaImagen = true;
-    public static bool   NuevosDatos = false;
-    public static Datos  DatosActuales;
-    public static Datos  DatosIniciales = null;
-    public        string port           = "COM6";
-    public        int    baudrate       = 9600;
-    private       Thread myThread;
+    public static bool NuevaImagen = true;
+    public static bool NuevosDatos = false;
+    public static Datos DatosActuales;
+    public static Datos DatosIniciales = null;
 
-    public List< Datos > DatosGuardados = new List< Datos >();
+    private Thread myThread;
+
+    public List<Datos> DatosGuardados = new List<Datos>();
 
     private void Start(){
         //serialPort = new SerialPort( PlayerPrefs.GetString(ComSelector.key), 9600 );
-        updatePort( port, baudrate );
+        updatePort(port, baudrate);
     }
 
-    private void updatePort( string portn, int baudraten ){
-        serialPort             = new SerialPort( portn, baudraten );
+    private void updatePort(string portn, int baudraten){
+        serialPort = new SerialPort(portn, baudraten);
         serialPort.ReadTimeout = 5000;
     }
 
     private void OnDisable(){
-        if( myThread != null && myThread.IsAlive ){
+        if (myThread != null && myThread.IsAlive){
             myThread.Abort();
         }
 
@@ -38,70 +40,70 @@ public class Comunicacion : MonoBehaviour{
     }
 
     private void Update(){
-        if( serialPort.IsOpen ){
+        if (serialPort.IsOpen){
             return;
         }
 
         try{
-            Debug.Log( "Tratando de abrir puerto serial" );
+            Debug.Log("Tratando de abrir puerto serial");
             serialPort.Open();
-            Debug.Log( "Listo" );
+            Debug.Log("Listo");
             // Iniciar el hilo
-            myThread = new Thread( hiloLectura );
+            myThread = new Thread(hiloLectura);
             myThread.Start();
         }
-        catch( Exception e ){
-            Debug.Log( e );
+        catch (Exception e){
+            Debug.Log(e);
         }
     }
 
     private void hiloLectura(){
-        while( true ){
-            if( serialPort.IsOpen == false ){
+        while (true){
+            if (serialPort.IsOpen == false){
                 return;
             }
 
             try{
                 Refresh();
             }
-            catch( Exception e ){
-                Debug.Log( e );
+            catch (Exception e){
+                Debug.Log(e);
                 serialPort.Close();
             }
         }
     }
 
     public void saveData(){
-        string path = "Assets/Logs/log_" + DateTime.Now.ToString( "yyyy-MM-dd_HH-mm-ss" ) + ".txt";
-        Debug.Log( "Guardando datos en " + path );
-        using( StreamWriter sw = File.CreateText( path ) ){
-            foreach( Datos d in DatosGuardados ){
+        string path = "Assets/Logs/log_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+        Debug.Log("Guardando datos en " + path);
+        using (StreamWriter sw = File.CreateText(path)){
+            foreach (Datos d in DatosGuardados){
                 sw.WriteLine(
-                    $"{d.temperaturaSht},"      +
-                    $"{d.temperaturaMpu},"      +
-                    $"{d.timestamp},"           +
-                    $"{d.altura},"              +
-                    $"{d.presion},"             +
-                    $"{d.humedad},"             +
-                    $"{d.Aceleracion.x},"       +
-                    $"{d.Aceleracion.y},"       +
-                    $"{d.Aceleracion.z},"       +
-                    $"{d.Gyro.x},"              +
-                    $"{d.Gyro.y},"              +
-                    $"{d.Gyro.z},"              +
-                    $"{d.Magnet.x},"            +
-                    $"{d.Magnet.y},"            +
-                    $"{d.Magnet.z},"            +
-                    $"{d.direccionBrujula},"    +
-                    $"{d.yaw},"                 +
-                    $"{d.pitch},"               +
-                    $"{d.roll},"                +
-                    $"{d.deltaFiltro},"         +
-                    $"{d.latitud},"             +
-                    $"{d.longitud},"            +
+                    $"{d.temperaturaSht}," +
+                    $"{d.temperaturaMpu}," +
+                    $"{d.timestamp}," +
+                    $"{d.altura}," +
+                    $"{d.presion}," +
+                    $"{d.humedad}," +
+                    $"{d.Aceleracion.x}," +
+                    $"{d.Aceleracion.y}," +
+                    $"{d.Aceleracion.z}," +
+                    $"{d.Gyro.x}," +
+                    $"{d.Gyro.y}," +
+                    $"{d.Gyro.z}," +
+                    $"{d.Magnet.x}," +
+                    $"{d.Magnet.y}," +
+                    $"{d.Magnet.z}," +
+                    $"{d.direccionBrujula}," +
+                    $"{d.yaw}," +
+                    $"{d.pitch}," +
+                    $"{d.roll}," +
+                    $"{d.deltaFiltro}," +
+                    $"{d.latitud}," +
+                    $"{d.longitud}," +
                     $"{d.velocidadHorizontal}," +
-                    $"{d.dilucionGps},"         +
-                    $"{d.tamañoImagen}"         +
+                    $"{d.dilucionGps}," +
+                    $"{d.tamañoImagen}" +
                     $"{d.bateria}"
                 );
             }
@@ -110,68 +112,68 @@ public class Comunicacion : MonoBehaviour{
 
     // Lee los datos seriales y realiza acciones en Unity
     private void Refresh(){
-        if( DatosRecibidos.tamañoImagen == 0 ){
-            string serialData = serialPort.ReadTo( "\n" );
-            if( serialData.Length == 0 ){
-                Debug.Log( "No hay datos" );
+        if (DatosRecibidos.tamañoImagen == 0){
+            string serialData = serialPort.ReadTo("\n");
+            if (serialData.Length == 0){
+                Debug.Log("No hay datos");
                 return;
             }
 
             // Procesa los datos recibidos (puedes realizar análisis y acciones aquí)
-            Debug.Log( "Datos seriales recibidos: " + serialData );
+            Debug.Log("Datos seriales recibidos: " + serialData);
             //Si el mensaje son los datos
-            string[] data = serialData.Substring( 1, serialData.Length - 2 ).Replace( " ", "" ).Split( ',' );
-            if( data.Length < 25 ){
-                Debug.Log( "Datos incompletos" );
+            string[] data = serialData.Substring(1, serialData.Length - 2).Replace(" ", "").Split(',');
+            if (data.Length < 25){
+                Debug.Log("Datos incompletos");
                 return;
             }
 
             DatosRecibidos = new Datos();
-            float.TryParse( data[0], out DatosRecibidos.temperaturaSht );
-            float.TryParse( data[1], out DatosRecibidos.temperaturaMpu );
+            float.TryParse(data[0], out DatosRecibidos.temperaturaSht);
+            float.TryParse(data[1], out DatosRecibidos.temperaturaMpu);
 
-            float.TryParse( data[2], out DatosRecibidos.timestamp );
+            float.TryParse(data[2], out DatosRecibidos.timestamp);
 
-            float.TryParse( data[3], out DatosRecibidos.altura );
-            float.TryParse( data[4], out DatosRecibidos.presion );
-            float.TryParse( data[5], out DatosRecibidos.humedad );
+            float.TryParse(data[3], out DatosRecibidos.altura);
+            float.TryParse(data[4], out DatosRecibidos.presion);
+            float.TryParse(data[5], out DatosRecibidos.humedad);
 
-            float.TryParse( data[6], out DatosRecibidos.Aceleracion.x );
-            float.TryParse( data[7], out DatosRecibidos.Aceleracion.y );
-            float.TryParse( data[8], out DatosRecibidos.Aceleracion.z );
+            float.TryParse(data[6], out DatosRecibidos.Aceleracion.x);
+            float.TryParse(data[7], out DatosRecibidos.Aceleracion.y);
+            float.TryParse(data[8], out DatosRecibidos.Aceleracion.z);
 
-            float.TryParse( data[9],  out DatosRecibidos.Gyro.x );
-            float.TryParse( data[10], out DatosRecibidos.Gyro.y );
-            float.TryParse( data[11], out DatosRecibidos.Gyro.z );
+            float.TryParse(data[9], out DatosRecibidos.Gyro.x);
+            float.TryParse(data[10], out DatosRecibidos.Gyro.y);
+            float.TryParse(data[11], out DatosRecibidos.Gyro.z);
 
-            float.TryParse( data[12], out DatosRecibidos.Magnet.x );
-            float.TryParse( data[13], out DatosRecibidos.Magnet.y );
-            float.TryParse( data[14], out DatosRecibidos.Magnet.z );
+            float.TryParse(data[12], out DatosRecibidos.Magnet.x);
+            float.TryParse(data[13], out DatosRecibidos.Magnet.y);
+            float.TryParse(data[14], out DatosRecibidos.Magnet.z);
 
-            float.TryParse( data[15], out DatosRecibidos.direccionBrujula );
+            float.TryParse(data[15], out DatosRecibidos.direccionBrujula);
 
-            float.TryParse( data[16], out DatosRecibidos.yaw );
-            float.TryParse( data[17], out DatosRecibidos.pitch );
-            float.TryParse( data[18], out DatosRecibidos.roll );
+            float.TryParse(data[16], out DatosRecibidos.yaw);
+            float.TryParse(data[17], out DatosRecibidos.pitch);
+            float.TryParse(data[18], out DatosRecibidos.roll);
 
-            float.TryParse( data[19], out DatosRecibidos.deltaFiltro );
+            float.TryParse(data[19], out DatosRecibidos.deltaFiltro);
 
-            float.TryParse( data[20], out DatosRecibidos.latitud );
-            float.TryParse( data[21], out DatosRecibidos.longitud );
+            float.TryParse(data[20], out DatosRecibidos.latitud);
+            float.TryParse(data[21], out DatosRecibidos.longitud);
 
-            float.TryParse( data[22], out DatosRecibidos.velocidadHorizontal );
+            float.TryParse(data[22], out DatosRecibidos.velocidadHorizontal);
 
-            float.TryParse( data[23], out DatosRecibidos.dilucionGps );
+            float.TryParse(data[23], out DatosRecibidos.dilucionGps);
 
-            int.TryParse( data[24], out DatosRecibidos.tamañoImagen );
+            int.TryParse(data[24], out DatosRecibidos.tamañoImagen);
 
-            int.TryParse( data[25], out DatosRecibidos.bateria );
+            int.TryParse(data[25], out DatosRecibidos.bateria);
             NuevosDatos = true;
-            if( DatosIniciales == null ){
+            if (DatosIniciales == null){
                 DatosIniciales = DatosRecibidos;
             }
 
-            DatosGuardados.Add( DatosRecibidos );
+            DatosGuardados.Add(DatosRecibidos);
         }
         else{
             /*
@@ -188,29 +190,29 @@ public class Comunicacion : MonoBehaviour{
 
             try{
                 byte[] imageData = new byte[DatosRecibidos.tamañoImagen];
-                int    x         = 0;
-                int    maxIter   = 1000;
-                int    iter;
-                for( int i = 0; i < DatosRecibidos.tamañoImagen; i++ ){
+                int x = 0;
+                int maxIter = 1000;
+                int iter;
+                for (int i = 0; i < DatosRecibidos.tamañoImagen; i++){
                     iter = 0;
 
                     do{
                         x = serialPort.ReadByte();
                         iter++;
-                        if( iter >= maxIter ) Debug.LogError( "Excedido el tiempo de espera para el serial !!" );
-                    } while( x == -1 );
+                        if (iter >= maxIter) Debug.LogError("Excedido el tiempo de espera para el serial !!");
+                    } while (x == -1);
 
                     imageData[i] = (byte)x;
                 }
 
-                Debug.Log( String.Format( "Buffer de {0} bytes leido", imageData.Length ) );
-                Debug.Log( BitConverter.ToString( imageData ) );
-                File.WriteAllBytes( "Assets/Imagenes/imagen_serial.jpg", imageData );
+                Debug.Log(String.Format("Buffer de {0} bytes leido", imageData.Length));
+                Debug.Log(BitConverter.ToString(imageData));
+                File.WriteAllBytes("Assets/Imagenes/imagen_serial.jpg", imageData);
                 DatosRecibidos.tamañoImagen = 0;
-                NuevaImagen                 = true;
+                NuevaImagen = true;
             }
-            catch( Exception e ){
-                Debug.Log( e.Message );
+            catch (Exception e){
+                Debug.Log(e.Message);
             }
         }
 
@@ -239,7 +241,7 @@ public class Datos{
 
     public float deltaFiltro = 0;
 
-    public float latitud  = 0;
+    public float latitud = 0;
     public float longitud = 0;
 
     public float velocidadHorizontal = 0;
@@ -254,10 +256,9 @@ public class Datos{
     public float vibracion = 0;
 
     public float horizontal = 0;
-    public float vertical   = 0;
+    public float vertical = 0;
 
     public int numeroSatelites = 0;
-    public int calidadSeñal    = 0;
-    public int señalRadio      = 0;
-
+    public int calidadSeñal = 0;
+    public int señalRadio = 0;
 }
